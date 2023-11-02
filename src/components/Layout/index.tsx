729dd63ecb7cc826f4b86cd6542cead7df7e8f0f
Harr-Yates <hyates1@gmail.com>
2023-11-02 19:17:23 +0100

    feat: :zap:

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,23 +6,40 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(true);
+  // Initialize darkMode state from localStorage or default to true
+  const [darkMode, setDarkMode] = useState(() => {
+    if (typeof window !== "undefined") {
+      const savedMode = localStorage.getItem("darkMode");
+      return savedMode === "true" ? true : savedMode === "false" ? false : true; // default to true if not set
+    }
+    return true; // default to true if window is not defined
+  });
 
   // Toggle dark mode
-  const toggleDarkMode = () => setDarkMode(!darkMode);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    // Save the new state to localStorage
+    localStorage.setItem("darkMode", (!darkMode).toString());
+  };
 
   // Apply the theme
   useEffect(() => {
     const body = document.body;
     const toggle = darkMode ? "dark" : "light";
     body.setAttribute("data-theme", toggle);
+    // Save the current state to localStorage
+    localStorage.setItem("darkMode", darkMode.toString());
   }, [darkMode]);
 
   return (
     <div className="layout">
       <div
         onClick={toggleDarkMode}
-        className="layout__dark-mode-toggle">
+        className="layout__dark-mode-toggle"
+        role="button" // Accessibility improvement
+        aria-pressed={darkMode} // Accessibility improvement
+        tabIndex={0} // Accessibility improvement
+      >
         {darkMode ? <FaSun /> : <FaMoon />}
       </div>
       <div className="layout__main">{children}</div>
